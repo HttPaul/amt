f98911acb0f65c9db840d5962ed544565fc979e8
Paul L <>
2021-05-23 22:57:46 +0000

    Included echo for each action.

--- main.sh
+++ main.sh
@@ -1,4 +1,15 @@
+#All My Tools for deploy on cloud machine
+#Last Updated: 2021/05/23
+
+echo "AMT script initiated...Please standby..."
+echo "Installing Terminator..."
 sudo add-apt-repository ppa:gnome-terminator
 sudo apt-get update
 sudo apt-get install terminator
+echo "...Terminator installed"
+
+echo "Cloning Reconbot..."
 git clone https://github.com/0bs3ssi0n/Reconbot.git
+
+
+echo "Script Complete!"
